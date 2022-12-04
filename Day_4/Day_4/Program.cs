9fb8b366f0df4cc48b8c34646b823e0ffdb9a187
Dániel Marcell <daniel.marcell171@gmail.com>
2022-12-04 19:37:44 +0100

    Add files via upload

---
+++ Day_4/Day_4/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day_4;
+Console.WriteLine("Hello, World!");
+Input input = new Input();
+
+Console.WriteLine("Overlap count: " + input.CalculateOverlappings());
+Console.WriteLine("Partial Overlap count: " + input.CalculatePartialOverlappings());
